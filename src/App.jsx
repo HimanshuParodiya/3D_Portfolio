6e752b46d33e7a4a0433bae72072d344626ba735
Himanshu <himanshuparodiya@gmail.com>
2023-11-21 13:16:52 +0530

    Add logo and menu items to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar } from "./Components/index";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
         {/* Navbar  */}
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" element={"Home"} />
           <Route path="/about" element={"About"} />
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+// File: index.js
+import Navbar from "./Navbar"
+
+
+export { Navbar }
