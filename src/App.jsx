0bc637323c189afe14ecae3f09b669faa74a2716
Himanshu <himanshuparodiya@gmail.com>
2023-11-21 12:30:26 +0530

    Setup React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div className="text-red-600">Let's Go </div>;
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        {/* Navbar  */}
+        Navbar
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/projects" element={"Projects"} />
+          <Route path="/contact" element={"Contact"} />
+        </Routes>
+      </Router>
+    </main>
+  );
 };
 
 export default App;
